93bc40fc24dd5aa5f732d994fcb0bf48fd6ff87f
pi6220na <pi6220na@gmail.com>
2017-11-30 07:00:56 -0600

    minor updates

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('JSnake', { title: 'Express' });   //was index  part1 JSnake
+  res.render('JSnakeTest', { title: 'Express' });   //was index  part1 JSnake
 });
 
 module.exports = router;
